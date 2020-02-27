e674dfad340f8f9d669164583234ffbb3df7d3a4
semivori <semivoriii@gmail.com>
2020-02-27 14:18:35 +0200

    Updates

---
+++ precache-manifest.340eb8f490da80311b765f3002b4dfbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aea348dda735d07f70cb92e5f6ee79d",
+    "url": "/learn-react---expense-tracker/index.html"
+  },
+  {
+    "revision": "d555394954525321d89e",
+    "url": "/learn-react---expense-tracker/static/css/main.97e366b4.chunk.css"
+  },
+  {
+    "revision": "7dd68a108f36bc335a6c",
+    "url": "/learn-react---expense-tracker/static/js/2.2002e6db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/learn-react---expense-tracker/static/js/2.2002e6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d555394954525321d89e",
+    "url": "/learn-react---expense-tracker/static/js/main.71c62c8f.chunk.js"
+  },
+  {
+    "revision": "066cdcc88797aad7cc17",
+    "url": "/learn-react---expense-tracker/static/js/runtime-main.49c7d73e.js"
+  }
+]);
